d1a22bdfab30dd3ea9d8ea040cce6adf567c6bd3
Oluwasanmi John <105114113+SanmiJohn@users.noreply.github.com>
2022-12-31 13:09:33 +0100

    Continuous Operation

--- app.js
+++ app.js
@@ -2,24 +2,49 @@ const IncomeButton = document.getElementById('incomebtn');
 const ExpenseButton = document.getElementById('expensebtn');
 let Description = document.getElementById('description')
 let Amount = document.getElementById('amount')
-let IncomeSpan = document.getElementById('incomespan')
-let AmountSpan = document.getElementById('amountspan')
 const IncomeAdded = document.getElementById('incomeadded')
 const ExpenseAdded = document.getElementById('expenseadded')
 
+IncomeButton.addEventListener('click', Income)
+ExpenseButton.addEventListener('click', Expense)
+
 function Income() {
-    IncomeAdded.style.backgroundColor = 'white'
+    let newIncomeDiv = document.createElement('div')
+    newIncomeDiv.style.backgroundColor = 'white'
+    newIncomeDiv.style.width = '16rem'
+    newIncomeDiv.style.height = '1.5rem'
+    newIncomeDiv.style.borderRadius = '1rem'
+    newIncomeDiv.style.marginBottom = '0.7rem'
+    newIncomeDiv.style.marginRight = '0.8rem'
+    newIncomeDiv.style.paddingTop = '0.3rem'
 
-    IncomeSpan.innerText = Description.value
+    let newDescriptionSpan = document.createElement('span')
+    newDescriptionSpan.style.marginRight = '4rem'
+    newDescriptionSpan.innerText = Description.value
+
+    let newAmountSpan = document.createElement('span')
+    newAmountSpan.style.marginLeft = '4rem'
+    newAmountSpan.style.color = '#007f5f'
+    newAmountSpan.innerText = Amount.value
+
+    newIncomeDiv.append(newDescriptionSpan)
+    newIncomeDiv.append(newAmountSpan)
+
+    IncomeAdded.append(newIncomeDiv)
     Description.value = ''
-
-    AmountSpan.innerText = Amount.value
     Amount.value = ''
 }
 
 function Expense() {
-    // ExpenseAdded.append(Now)
+    let newExpenseDiv = document.createElement('div')
+    newExpenseDiv.style.backgroundColor = 'white'
+    newExpenseDiv.style.width = '16rem'
+    newExpenseDiv.style.height = '1.5rem'
+    newExpenseDiv.style.borderRadius = '1rem'
+    newExpenseDiv.style.marginBottom = '0.7rem'
+    newExpenseDiv.style.marginLeft = '0.6rem'
+    newExpenseDiv.style.paddingTop = '0.3rem'
+    ExpenseAdded.append(newExpenseDiv)
+    Description.value = ''
+    Amount.value = ''
 }
-
-IncomeButton.addEventListener('click', Income)
-ExpenseButton.addEventListener('click', Expense) 
