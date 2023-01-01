95f614fe1a4286c90097bca9f95c4eb89d25af99
Oluwasanmi John <105114113+SanmiJohn@users.noreply.github.com>
2023-01-01 12:06:08 +0100

    Update JavaScript
    Added the If and Else statements to the code.  Whereby if only Description or Amount is inputted, there's an alert to bring the missing details to the notice of the user.

--- app.js
+++ app.js
@@ -9,42 +9,72 @@ IncomeButton.addEventListener('click', Income)
 ExpenseButton.addEventListener('click', Expense)
 
 function Income() {
-    let newIncomeDiv = document.createElement('div')
-    newIncomeDiv.style.backgroundColor = 'white'
-    newIncomeDiv.style.width = '16rem'
-    newIncomeDiv.style.height = '1.5rem'
-    newIncomeDiv.style.borderRadius = '1rem'
-    newIncomeDiv.style.marginBottom = '0.7rem'
-    newIncomeDiv.style.marginRight = '0.8rem'
-    newIncomeDiv.style.paddingTop = '0.3rem'
+    if (Description.value && Amount.value) {
+        let newIncomeDiv = document.createElement('div')
+        newIncomeDiv.style.backgroundColor = 'white'
+        newIncomeDiv.style.width = '16rem'
+        newIncomeDiv.style.height = '1.5rem'
+        newIncomeDiv.style.borderRadius = '1rem'
+        newIncomeDiv.style.marginBottom = '0.7rem'
+        newIncomeDiv.style.marginRight = '0.8rem'
+        newIncomeDiv.style.paddingTop = '0.3rem'
 
-    let newDescriptionSpan = document.createElement('span')
-    newDescriptionSpan.style.marginRight = '4rem'
-    newDescriptionSpan.innerText = Description.value
+        let newDescriptionSpan = document.createElement('span')
+        newDescriptionSpan.style.marginRight = '4rem'
+        newDescriptionSpan.innerText = Description.value
 
-    let newAmountSpan = document.createElement('span')
-    newAmountSpan.style.marginLeft = '4rem'
-    newAmountSpan.style.color = '#007f5f'
-    newAmountSpan.innerText = Amount.value
+        let newAmountSpan = document.createElement('span')
+        newAmountSpan.style.marginLeft = '4rem'
+        newAmountSpan.style.color = '#007f5f'
+        newAmountSpan.innerText = Amount.value
 
-    newIncomeDiv.append(newDescriptionSpan)
-    newIncomeDiv.append(newAmountSpan)
+        newIncomeDiv.append(newDescriptionSpan)
+        newIncomeDiv.append(newAmountSpan)
 
-    IncomeAdded.append(newIncomeDiv)
-    Description.value = ''
-    Amount.value = ''
+        IncomeAdded.append(newIncomeDiv)
+        Description.value = ''
+        Amount.value = ''
+    } else if (Description.value) {
+        alert('Input Amount')
+    } else if (Amount.value) {
+        alert('Input Description')
+    } else {
+        alert('Input Description and Amount')
+    }
 }
 
 function Expense() {
-    let newExpenseDiv = document.createElement('div')
-    newExpenseDiv.style.backgroundColor = 'white'
-    newExpenseDiv.style.width = '16rem'
-    newExpenseDiv.style.height = '1.5rem'
-    newExpenseDiv.style.borderRadius = '1rem'
-    newExpenseDiv.style.marginBottom = '0.7rem'
-    newExpenseDiv.style.marginLeft = '0.6rem'
-    newExpenseDiv.style.paddingTop = '0.3rem'
-    ExpenseAdded.append(newExpenseDiv)
-    Description.value = ''
-    Amount.value = ''
+    if (Description.value && Amount.value) {
+        let newExpenseDiv = document.createElement('div')
+        newExpenseDiv.style.backgroundColor = 'white'
+        newExpenseDiv.style.width = '16rem'
+        newExpenseDiv.style.height = '1.5rem'
+        newExpenseDiv.style.borderRadius = '1rem'
+        newExpenseDiv.style.marginBottom = '0.7rem'
+        newExpenseDiv.style.marginLeft = '0.6rem'
+        newExpenseDiv.style.paddingTop = '0.3rem'
+
+        let newDescriptionSpan = document.createElement('span')
+        newDescriptionSpan.style.marginRight = '4rem'
+        newDescriptionSpan.innerText = Description.value
+
+        let newAmountSpan = document.createElement('span')
+        newAmountSpan.style.marginLeft = '4rem'
+        newAmountSpan.style.color = '#007f5f'
+        newAmountSpan.innerText = Amount.value
+
+        newExpenseDiv.append(newDescriptionSpan)
+        newExpenseDiv.append(newAmountSpan)
+
+        ExpenseAdded.append(newExpenseDiv)
+        Description.value = ''
+        Amount.value = ''
+    }
+    else if (Description.value) {
+        alert('Input Amount')
+    } else if (Amount.value) {
+        alert('Input Description')
+    } else {
+        alert('Input Description and Amount')
+    }
 }
